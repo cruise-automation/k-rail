82a496e63988984abfdfaa09391727825bb2ff93
Dustin <dustindecker@protonmail.com>
2019-11-03 18:41:06 -0800

    Add handler + policy + exemption integration benchmarks

--- server/webhook_test.go
+++ server/webhook_test.go
@@ -16,6 +16,8 @@ import (
 	"encoding/json"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/cruise-automation/k-rail/policies"
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
 	authenticationv1 "k8s.io/api/authentication/v1"
@@ -24,7 +26,15 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-func TestServer_validateResources(t *testing.T) {
+type test struct {
+	name              string
+	resourceName      string
+	resourceNamespace string
+	podSpec           corev1.PodSpec
+	allow             bool
+}
+
+func test_setup() (Server, []test) {
 	rawExemptions := []policies.RawExemption{
 		{
 			ResourceName:   "test-resource",
@@ -50,15 +60,11 @@ func TestServer_validateResources(t *testing.T) {
 		Exemptions: compiledExemptions,
 	}
 
+	log.SetLevel(log.ErrorLevel)
+
 	testSrv.registerPolicies()
 
-	tests := []struct {
-		name              string
-		resourceName      string
-		resourceNamespace string
-		podSpec           corev1.PodSpec
-		allow             bool
-	}{
+	tests := []test{
 		{
 			name:  "deny by policy",
 			allow: false,
@@ -83,6 +89,61 @@ func TestServer_validateResources(t *testing.T) {
 			},
 		},
 	}
+
+	return testSrv, tests
+}
+
+func runBenchmark(b *testing.B, testSrv Server, tt test) {
+	pod := corev1.Pod{Spec: tt.podSpec}
+	pod.Name = tt.resourceName
+	raw, _ := json.Marshal(pod)
+	ar := admissionv1beta1.AdmissionReview{
+		Request: &admissionv1beta1.AdmissionRequest{
+			Namespace: tt.resourceNamespace,
+			Object:    runtime.RawExtension{Raw: raw},
+			Resource:  metav1.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"},
+			UserInfo:  authenticationv1.UserInfo{Groups: []string{"group1"}},
+		},
+	}
+
+	if got := testSrv.validateResources(ar); got.Response.Allowed != tt.allow {
+		b.Errorf("Server.validateResources() = %v, want %v", got.Response.Allowed, tt.allow)
+	}
+}
+
+func BenchmarkServer_DenyPolicy(b *testing.B) {
+	testSrv, tests := test_setup()
+	tt := tests[0]
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			runBenchmark(b, testSrv, tt)
+		}
+	})
+}
+
+func BenchmarkServer_AllowPolicy(b *testing.B) {
+	testSrv, tests := test_setup()
+	tt := tests[1]
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			runBenchmark(b, testSrv, tt)
+		}
+	})
+}
+
+func BenchmarkServer_AllowExemption(b *testing.B) {
+	testSrv, tests := test_setup()
+	tt := tests[2]
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			runBenchmark(b, testSrv, tt)
+		}
+	})
+}
+
+func TestServer_validateResources(t *testing.T) {
+	testSrv, tests := test_setup()
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			pod := corev1.Pod{Spec: tt.podSpec}
