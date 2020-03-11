af4266c1eb1b2e307befe127454df4b1f9ff14c6
Dustin Decker <dustindecker@protonmail.com>
2020-03-11 13:08:58 -0700

    remove unused import

--- policies/ingress/unique_ingress_host.go
+++ policies/ingress/unique_ingress_host.go
@@ -22,7 +22,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 func NewPolicyRequireUniqueHost() (PolicyRequireUniqueHost, error) {
