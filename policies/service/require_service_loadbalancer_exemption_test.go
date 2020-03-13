09824604d791a2df5ceb1ae044b340e35bbd44ed
Bernard Van De Walle <bernard.vandewalle@getcruise.com>
2020-03-13 15:33:30 -0700

    Adding tests for services

---
+++ policies/service/require_service_loadbalancer_exemption_test.go
@@ -0,0 +1,86 @@
+// Copyright 2019 Cruise LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    https://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/cruise-automation/k-rail/policies"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func TestPolicyRequireServiceLoadbalancerExemption_Validate(t *testing.T) {
+	ctx := context.Background()
+
+	tests := []struct {
+		name       string
+		service    *corev1.Service
+		violations int
+	}{
+		{
+			name:       "annotation present, no violation",
+			violations: 0,
+			service: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"cloud.google.com/load-balancer-type": "internal",
+					},
+				},
+			},
+		},
+		{
+			name:       "annotation present but bad annotation, violation",
+			violations: 1,
+			service: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"cloud.google.com/load-balancer-type": "whatever123",
+					},
+				},
+			},
+		},
+		{
+			name:       "no annotation present, violation",
+			violations: 1,
+			service: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var ar = &admissionv1beta1.AdmissionRequest{}
+
+			raw, _ := json.Marshal(tt.service)
+			ar = &admissionv1beta1.AdmissionRequest{
+				Namespace: "namespace",
+				Name:      "name",
+				Object:    runtime.RawExtension{Raw: raw},
+				Resource:  metav1.GroupVersionResource{Group: "core", Version: "v1", Resource: "services"},
+			}
+
+			v := PolicyRequireServiceLoadbalancerExemption{}
+			if got, _ := v.Validate(ctx, policies.Config{PolicyRequireServiceLoadBalancerType: []string{"internal"}}, ar); !reflect.DeepEqual(len(got), tt.violations) {
+				t.Errorf("PolicyRequireServiceLoadbalancerExemption() %s got %v want %v violations", tt.name, len(got), tt.violations)
+			}
+		})
+	}
+}
