3e804486df23b3be15570bee5cf7cd274b71624e
shellw <shelley.wu@getcruise.com>
2020-03-29 17:27:02 -0700

    Add persistent volume resource

---
+++ resource/persistent_volume.go
@@ -0,0 +1,53 @@
+// Copyright 2019 Cruise LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    https://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resource
+
+import (
+	"context"
+
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// PersistentVolumeResource contains the information needed for processing by a Policy
+type PersistentVolumeResource struct {
+	PersistentVolume corev1.PersistentVolume
+	ResourceName     string
+	ResourceKind     string
+}
+
+// GetPersistentVolumeResource extracts and PersistentVolumeResource from an AdmissionRequest
+func GetPersistentVolumeResource(ctx context.Context, ar *admissionv1beta1.AdmissionRequest) *PersistentVolumeResource {
+	c := GetResourceCache(ctx)
+	return c.getOrSet(cacheKeyPersistentVolume, func() interface{} {
+		return decodePersistentVolumeResource(ar)
+	}).(*PersistentVolumeResource)
+}
+
+func decodePersistentVolumeResource(ar *admissionv1beta1.AdmissionRequest) *PersistentVolumeResource {
+	switch ar.Resource {
+	case metav1.GroupVersionResource{Group: "core", Version: "v1", Resource: "persistentvolumes"}:
+		pv := corev1.PersistentVolume{}
+		if err := decodeObject(ar.Object.Raw, &pv); err != nil {
+			return nil
+		}
+		return &PersistentVolumeResource{
+			PersistentVolume: pv,
+			ResourceName:     GetResourceName(pv.ObjectMeta),
+			ResourceKind:     "PersistentVolume",
+		}
+	default:
+		return nil
+	}
+}
